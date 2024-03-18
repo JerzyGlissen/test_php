d71bca1346ceabaa757fc2d169dc5b50a67d36b5
JerzyGlissen <56402795+JerzyGlissen@users.noreply.github.com>
2024-03-18 21:46:53 +0100

    Add files via upload

---
+++ git_hub_test_PHP.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>php_github_test</title>
+<style>
+body {
+	padding:5%;
+	text-align:center;
+}
+</style>
+</head>
+<body>
+<?php
+
+echo "<h2>Tótai jest php HOGH !</h2>";
+
+?>
+</body>
+</html?
